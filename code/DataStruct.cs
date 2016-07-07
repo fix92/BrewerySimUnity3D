7c39dd37472152974112117a9085440c900fd1d6
Felix Weissl <felix.weissl@tum.de>
2016-07-07 05:05:48 +0200

    small changes

---
+++ code/DataStruct.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Units;
+
+[System.Serializable]
+public class DataStruct : System.Object
+{
+	public int id;
+	public Unit1 Unit1;
+	public Unit2 Unit2;
+	public Unit3 Unit3;
+	public Unit4 Unit4;
+	public Unit5 Unit5;
+	public Unit6 Unit6;
+
+	public override string ToString()
+	{
+		return "id: " + id + " || " + Unit1 + " || " + Unit2 + " || " + Unit3 + " || " + Unit4 + " || " + Unit5 + " || " + Unit6;
+	}
+}
---
+++ code/MoveUp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUp : MonoBehaviour {
+
+	bool rotateOn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotateOn) {
+			transform.Rotate(Vector3.up * Time.deltaTime*20);
+		}
+	}
+
+	public void startRotating() {
+		rotateOn = true;
+	}
+	public void stopRotating() {
+		rotateOn = false;
+	}
+
+}
